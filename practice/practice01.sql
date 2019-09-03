10a4a2fcccd5cf15ee8b248c4e8c53d5445246a9
BIT <BIT@DESKTOP-Q04GE57>
2019-09-03 17:30:39 +0900

    practice01 완료

--- practice/practice01.sql
+++ practice/practice01.sql
@@ -18,19 +18,19 @@ select count(to_date) as '직원 수' from titles group by to_date having to_dat
 select count(dept_name) as '부서 수' from departments;
 
 -- 6) 현재 부서 매니저는 몇 명이나 있나요?(역임 매너저는 제외)
-select count(emp_no) as '매니저 수' from dept_manager;
+select count(emp_no) as '매니저 수' from dept_manager where to_date like '9999%';
 
 -- 7) 전체 부서를 출력하려고 합니다. 순서는 부서이름이 긴 순서대로 출력해 보세요.
 select dept_name from departments order by length(dept_name) desc;
 
 -- 8) 현재 급여가 120,000이상 받는 사원은 몇 명이나 있습니까?
-select count(salary) as '명' from salaries where salary >= 120000;
+select count(distinct(emp_no)) as '명' from salaries where salary >= 120000 and to_date like '9999%';
 
 -- 9) 어떤 직책들이 있나요? 중복 없이 이름이 긴 순서대로 출력해 보세요.
 select distinct(title) from titles order by length(title) desc;
 
 -- 10) 현재 Enginner 직책의 사원은 총 몇 명입니까?
-select count(title) from titles where title like '%engineer';
+select count(title) from titles where title like '%engineer%';
 
 -- 11) 사번이 13250(Zeydy)인 지원이 직책 변경 상황을 시간순으로 출력해보세요.
 select emp_no as '사번', title as '직책', from_date as '입사일', to_date as '변경일' from titles where emp_no = '13250' order by from_date;
