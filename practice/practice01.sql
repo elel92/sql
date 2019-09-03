5b78828213aa499c064aae965a648bae53b54fd0
BIT <BIT@DESKTOP-Q04GE57>
2019-09-03 17:42:43 +0900

    sql prac01 과제 완료.

--- practice/practice01.sql
+++ practice/practice01.sql
@@ -6,7 +6,7 @@ select concat(first_name, ' ',last_name) as '이름' from employees where emp_no
 -- 2) 전체직원의 다음 정보를 조회하세요.
 --    가장 선임부터 출력이 되도록 하세요.
 --    출력은 이름, 성별,  입사일 순서이고 “이름”, “성별”, “입사일”로 컬럼 이름을 대체해 보세요.
-select concat(first_name, ' ',last_name) as '이름', gender as '성별', hire_date as '입사일' from employees order by hire_date asc;
+select concat(first_name, ' ', last_name) as '이름', gender as '성별', hire_date as '입사일' from employees order by hire_date asc;
 
 -- 3) 여직원과 남직원은 각 각 몇 명이나 있나요?
 select (select count(gender) from employees group by gender having gender = 'm') as '남성', (select count(gender) from employees group by gender having gender = 'f') as '여성';
